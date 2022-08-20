86f171e788ecfaba5ff30fb5b0a025db5ec2895c
#SG88 <101992385+SagaLove@users.noreply.github.com>
2022-08-20 21:32:25 +0000

    Add files via upload

---
+++ qiwwid/5.py
@@ -0,0 +1,21 @@
+from colorama import Fore
+
+print(f""" {Fore.MAGENTA}
+                                                                                                             
+     QQQQQQQQQ                                                iiii          tttt         hhhhhhh             
+   QQ:::::::::QQ                                             i::::i      ttt:::t         h:::::h             
+ QQ:::::::::::::QQ                                            iiii       t:::::t         h:::::h             
+Q:::::::QQQ:::::::Q                                                      t:::::t         h:::::h             
+Q::::::O   Q::::::Qwwwwwww           wwwww           wwwwwwwiiiiiiittttttt:::::ttttttt    h::::h hhhhh       
+Q:::::O     Q:::::Q w:::::w         w:::::w         w:::::w i:::::it:::::::::::::::::t    h::::hh:::::hhh    
+Q:::::O     Q:::::Q  w:::::w       w:::::::w       w:::::w   i::::it:::::::::::::::::t    h::::::::::::::hh  
+Q:::::O     Q:::::Q   w:::::w     w:::::::::w     w:::::w    i::::itttttt:::::::tttttt    h:::::::hhh::::::h 
+Q:::::O     Q:::::Q    w:::::w   w:::::w:::::w   w:::::w     i::::i      t:::::t          h::::::h   h::::::h
+Q:::::O     Q:::::Q     w:::::w w:::::w w:::::w w:::::w      i::::i      t:::::t          h:::::h     h:::::h
+Q:::::O  QQQQ:::::Q      w:::::w:::::w   w:::::w:::::w       i::::i      t:::::t          h:::::h     h:::::h
+Q::::::O Q::::::::Q       w:::::::::w     w:::::::::w        i::::i      t:::::t    tttttth:::::h     h:::::h
+Q:::::::QQ::::::::Q        w:::::::w       w:::::::w        i::::::i     t::::::tttt:::::th:::::h     h:::::h
+ QQ::::::::::::::Q          w:::::w         w:::::w         i::::::i     tt::::::::::::::th:::::h     h:::::h
+   QQ:::::::::::Q            w:::w           w:::w          i::::::i       tt:::::::::::tth:::::h     h:::::h
+     QQQQQQQQ::::QQ           www             www           iiiiiiii         ttttttttttt  hhhhhhh     hhhhhhh
+""")
